7a9d2b1dd72fdbd2923a774de9188146733375b2
Venkata Revanth VARDINENI <vvr2211@umd.edu>
2026-04-07 21:44:57 -0400

    feat(lib): extract fist marker colors and sphere radius

---
+++ src/lib/markerConstants.ts
@@ -0,0 +1,7 @@
+import { HAND_NZ_WORLD_SCALE } from './visionDriverConstants'
+
+export { HAND_NZ_WORLD_SCALE as FIST_MARKER_NZ_SCALE }
+
+export const FIST_DOT_RADIUS = 0.028
+export const LEFT_FIST_COLOR = '#44ddff'
+export const RIGHT_FIST_COLOR = '#ff66aa'
